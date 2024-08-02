a10f0659749732f88f248aaf5e2120d03d4b2f45
mohamed khial <126480908+mokhial27@users.noreply.github.com>
2024-08-02 19:42:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
 app.use('/stories', require('./routes/stories'))
 
-    const PORT = process.env.PORT
+    const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} and proccess of ${process.env.NODE_ENV})`);
